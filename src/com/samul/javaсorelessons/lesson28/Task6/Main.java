dea30883f925583be9e78e52e5e0b289f052b67a
SamulVlad <55761105+SamulVlad@users.noreply.github.com>
2024-08-01 09:51:34 +0300

    Add files via upload

--- src/com/samul/javaсorelessons/lesson28/Task6/Main.java
+++ src/com/samul/javaсorelessons/lesson28/Task6/Main.java
@@ -1,9 +1,43 @@
 package com.samul.javaсorelessons.lesson28.Task6;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Runnable run = new Runnable() {
+
+			@Override
+			public void run() {
+				Random rnd = new Random();
+				int willSleep = rnd.nextInt(5000);
+				System.out.println("Thread id : " + Thread.currentThread().getId() + " Will sleep: " + willSleep);
+
+				for (int i = 1; i <= 5; i++) {
+
+					System.out.println("Thread id : " + Thread.currentThread().getId() + "  Count: " + i);
+
+					try {
+						Thread.sleep(willSleep);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+				System.out.println("Thread id : " + Thread.currentThread().getId() + " Thread done. ");
+
+			}
+
+		};
+
+		Thread thr1 = new Thread(run);
+		thr1.start();
+
+		Thread thr2 = new Thread(run);
+		thr2.start();
+
+		Thread thr3 = new Thread(run);
+		thr3.start();
 
 	}
 
